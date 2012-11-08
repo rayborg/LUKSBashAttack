6b2b2803e944e2e02689b77080c05aa68759fa61
Raymond C. Borges Hink <borgesraymond@gmail.com>
2012-11-08 01:33:38 -0500

    Copyrights updated

--- dictionaryAttack.sh
+++ dictionaryAttack.sh
@@ -1,5 +1,22 @@
 #! /bin/bash
-#script to do a dictionary attack on LUKS
+#Copyright 2012, Raymond C. Borges Hink
+#This program is copyright under GNU GENERAL PUBLIC LICENSE Version 3
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#		~~~End of Copying permission statement~~~
+#This is a script performs a dictionary attack on a LUKS encrypted volume
 
 #read input
 echo Please enter LUKS volume full path 
